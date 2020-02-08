058d7b7716268651f70418549eda867b700eeecc
Juergen Edelbluth <edelbluth@users.noreply.github.com>
2020-02-08 20:16:24 +0100

    Fixing two pylint exceptions

--- hub_app/templatetags/hub_app_lang.py
+++ hub_app/templatetags/hub_app_lang.py
@@ -5,9 +5,15 @@ Template Tags for language handling
 from django import template
 from django.utils import translation
 
-register = template.Library()
+register = template.Library()  # pylint: disable=invalid-name
 
 
 @register.simple_tag(name='current_language')
-def do_current_language():
+def do_current_language() -> str:
+    """
+    Get the current language code
+
+    :rtype: str
+    :returns: The current language code
+    """
     return translation.get_language()
