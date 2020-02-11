39382b4f80aa5ec55f2aeb34094b465ce4200eb6
Juergen Edelbluth <edelbluth@users.noreply.github.com>
2020-02-11 18:09:07 +0100

    Moving hub_app to the top
    - needed to override the Django default 'createuser' command
    - single commit to make sure tests are still running and there are no side effects

--- hub/settings.py
+++ hub/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hub_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hub_app',
 ]
 
 MIDDLEWARE = [
