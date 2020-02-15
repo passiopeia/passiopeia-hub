0f598e649e3bf9b09d2293b20dcb50a79c0abb46
Juergen Edelbluth <edelbluth@users.noreply.github.com>
2020-02-15 16:21:43 +0100

    Refactoring Settings

--- hub/settings.py
+++ hub/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.contrib import messages
 from django.urls import reverse_lazy
 
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
