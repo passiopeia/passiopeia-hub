557e8c0253311301a4dc178ce759cffec36e0660
Juergen Edelbluth <edelbluth@users.noreply.github.com>
2020-02-16 21:15:20 +0100

    Management Command for cleaning pending registrations
    - Pending registrations are valid for 3 days
    - The command deletes ones that are older

---
+++ hub_app/management/commands/cleanpendingregistrations.py
@@ -0,0 +1,35 @@
+"""
+Clean up pending registrations from database
+
+You should use a cron job to trigger this regularly.
+"""
+import datetime
+
+from django.core.management import BaseCommand
+from django.utils.timezone import now
+
+from django.utils.translation import gettext_lazy as _
+
+from hub_app.models import BurnedOtp, HubUser
+
+
+class Command(BaseCommand):
+    """
+    Management Command for cleaning up expired pending registrations
+    """
+
+    help = _('Delete pending registrations older than 3 days')
+
+    def handle_clean_pending_registrations(self):
+        """
+        Remove pending registrations that are older than 3 days and the users associated with them
+        """
+        expired_user_registrations = HubUser.objects.filter(pendingregistration__valid_until__lt=now())
+        count = expired_user_registrations.count()
+        self.stdout.write(_('Deleting %(count)s user accounts with expired registrations.') % {'count': count})
+        if count > 0:
+            expired_user_registrations.delete()
+        self.stdout.write(self.style.SUCCESS(_('Done')))
+
+    def handle(self, *args, **options):
+        self.handle_clean_pending_registrations()
---
+++ hub_app/tests/test_command_cleanpendingregistrations.py
@@ -0,0 +1,53 @@
+"""
+Tests for the "cleanpendingregistrations" command
+"""
+from datetime import timedelta
+from io import StringIO
+
+from django.core.management import call_command
+from django.test import TestCase
+from django.utils.timezone import now
+
+from hub_app.management.commands import cleanpendingregistrations
+from hub_app.models import HubUser, PendingRegistration
+
+
+class DeleteOnFullDatabaseTest(TestCase):
+    """
+    Test on a full database
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        for i in range(100):
+            HubUser.objects.create_user(username='real_user_{}'.format(i))
+            user_pending_current = HubUser.objects.create_user(username='pending_current_{}'.format(i))
+            PendingRegistration.objects.create(user=user_pending_current)
+            user_pending_old = HubUser.objects.create_user(username='expired_pending_current_{}'.format(i))
+            expired_pending = PendingRegistration.objects.create(user=user_pending_old)
+            expired_pending.valid_until = now() - timedelta(seconds=1)
+            expired_pending.save()
+
+    def test_deletion_on_populated_database(self):
+        """
+        Test on populated database
+        """
+        self.assertEqual(300, HubUser.objects.all().count())
+        with StringIO() as out:
+            call_command(cleanpendingregistrations.Command(), stdout=out)
+            self.assertRegex(out.getvalue().strip(), r'(Done).{0,10}$')
+        self.assertEqual(200, HubUser.objects.all().count())
+
+
+class DeleteOnEmptyDatabaseTest(TestCase):
+    """
+    Run on an empty database
+    """
+
+    def test_delete_on_empty(self):
+        """
+        We are expecting no exception
+        """
+        with StringIO() as out:
+            call_command(cleanpendingregistrations.Command(), stdout=out)
+            self.assertRegex(out.getvalue().strip(), r'(Done).{0,10}$')
