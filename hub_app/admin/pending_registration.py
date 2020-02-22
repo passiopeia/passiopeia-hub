d333c398791458955552f8a266cdb972eb5c1f0f
Juergen Edelbluth <edelbluth@users.noreply.github.com>
2020-02-22 22:41:40 +0100

    Findings from PyLint

--- hub_app/admin/pending_registration.py
+++ hub_app/admin/pending_registration.py
@@ -10,30 +10,18 @@ class PendingRegistrationAdmin(admin.ModelAdmin):
     Pending Registration Admin
     """
 
-    list_display = (
-        'uuid',
-        'user',
-        'created',
-        'valid_until',
-    )
+    list_display = ('uuid', 'user', 'created', 'valid_until',)
 
     ordering = ('uuid',)
 
-    readonly_fields = (
-        'uuid',
-    )
+    readonly_fields = ('uuid', 'key',)
 
     fieldsets = (
-        (_('Base Data'), {
-            'fields': (
-                'uuid', 'user',
-                'created', 'valid_until',
-            ),
-        }),
+        (_('Base Data'), {'fields': ('uuid', 'user', 'created', 'valid_until',)}),
         (_('Key'), {'fields': ('key',)}),
     )
 
     def get_readonly_fields(self, request, obj=None):  # pragma: no cover  # No end-user use-case for that
         if obj:
-            return self.readonly_fields + ('user', 'created', 'key',)
+            return self.readonly_fields + ('user', 'created', 'key')
         return self.readonly_fields
